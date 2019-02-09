52f85531042cfcce1d6582692c768284762e99e2
jjescuderv <47001432+jjescuderv@users.noreply.github.com>
2019-02-08 22:47:29 -0500

    Update Taller3

--- talleres/taller03/Taller3.java
+++ talleres/taller03/Taller3.java
@@ -4,65 +4,63 @@ import java.util.Collections;
 /**
  * Clase en la cual se implementan los metodos del Taller 3
  *
- * @author Juan Camilo Guerrero, Santiago Pulgarin, Juan Escudero
+ * @author Juan Camilo Guerrero, Santiago Pulgarin, Juan José Escudero
  */
-public class Taller3 {
-
-
-    /**
-     * Metodo auxiliar para llamar el metodo posterior
-     *
-     * @param n numero de reinas
-     * @return numero de soluciones
-     */
+public class Taller3 {    
     public static void nReinas(int n) {
         nReinas(n, new int[n], 0);
     }
 
-
     private static void nReinas(int n, int[] tablero, int c) {
-        if (c == n) {
-            if (seAtacanHastaIoNo(tablero,n))
-            imprimirTablero(tablero);
-
+        if(c == n) {
+            if(!seAtacanHastaIoNo(tablero, c)) {
+                imprimirTablero(tablero);
+            }
         } else {
-            for (int i = 0; i < n ; i++) {
+            for (int i = 0; i < n; i++) {
                 tablero[c] = i;
-                if (seAtacanHastaIoNo(tablero,c)){
-                    nReinas(n,tablero,c + 1);
+                if(!seAtacanHastaIoNo(tablero, c)) {
+                    nReinas(n, tablero, c+1);
                 }
             }
-
         }
-
     }
 
-    private static boolean seAtacanHastaIoNo (int [] tablero,int elI ) {
-        for (int i=0; i<elI - 1; i++)
-            for (int j=i+1; j<elI; j++)
-                if (Math.abs (tablero[i] - tablero[j]) == Math.abs(i - j) || tablero[i]== tablero[j])
-                    return false;
-        return true;
+    private static boolean seAtacanHastaIoNo(int[] tablero, int c) {
+        for(int i = 0; i < c-1; i++) {
+            for(int j = i+1; j < c; j++) {
+                if(pendiente(i, j, tablero[i], tablero[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private static boolean pendiente(double x1, double x2, double y1, double y2) {
+        if(((y2-y1)/(x2-x1) == -1) || ((y2-y1)/(x2-x1) == 0) || ((y2-y1)/(x2-x1) == 1)) 
+            return true;
+        return false;
     }
 
     public static void imprimirTablero(int[] tablero) {
         int n = tablero.length;
         System.out.print("    ");
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             System.out.print(i + " ");
         }
         System.out.println("\n");
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             System.out.print(i + "   ");
-            for (int j = 0; j < n; ++j) {
+            for (int j = 0; j < n; j++) {
                 System.out.print((tablero[i] == j ? "Q" : "#") + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
-
+    
     public static void main(String[] args) {
-        nReinas(4);
+        nReinas(6);
     }
 }
